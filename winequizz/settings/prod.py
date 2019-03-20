6a99820fdea8351b35a8c3234eb6689930ced4ee
Amaury Carrade <amaury@carrade.eu>
2019-03-20 16:30:00 +0100

    MySQL STRICT mode

--- winequizz/settings/prod.py
+++ winequizz/settings/prod.py
@@ -30,5 +30,8 @@ DATABASES = {
         "PASSWORD": db_credentials["password"],
         "HOST": db_credentials["host"],
         "PORT": db_credentials["port"],
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_ALL_TABLES'"
+        }
     }
 }
